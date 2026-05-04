07ef30a5913e07a44ebcc334a78e4f4a90c35660
muqtasidbhatti <muqtasidbhatti246@gmail.com>
2026-05-05 03:24:39 +0500

    fix: resolve CORS issue on Vercel deployment

--- server.js
+++ server.js
@@ -4,7 +4,12 @@ const connectDB = require('./config/db')
 const cors = require('cors')
 
 const app = express()
-app.use(cors())
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}))
+app.options('*', cors())
 app.use(express.json())
 
 connectDB()
